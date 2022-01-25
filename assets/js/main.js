a7fabff8675da199a2c1d5ba209bdaeb80810399
islamnashaat03 <islamnashaat03@gmail.com>
2022-01-25 19:01:26 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,35 @@
-$('.box').draggable({
-  // snap:true,
-  containment: "body"
-});
+$(document).ready(function(){
+  $('.img-wrapper').draggable({
+    scroll:false,
+    // snap:true,
+    containment: "#dashboard",
+    // addClasses:false,
+    // snapMode: "inner",
+    snap:true
+    // appendTo :'body'
+  });
+  $('.img-wrapper img').resizable({
+    aspectRatio: true,
+    // disabled: true
+    containment: ".boxes .box",
+    animate:true,
+    ghost:true,
+    
+  });
+  $('.boxes .box').droppable({
+    accecpt:'.img-wrapper img',
+    // classes: {
+    //   "ui-droppable-active": "ui-state-active",
+    //   "ui-droppable-hover": "ui-state-hover"
+    // },
+    // drop:function( event, ui ){
+    //   $('.boxes').forEach(box => {
+    //     box.addClass( ".ui-droppable-active" )
+    //   });
+
+    // },
+    // animate:true,
+    // ghost:true,
+  });
+  
+})
